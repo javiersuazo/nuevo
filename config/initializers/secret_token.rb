0641b9a1cfc37a91611c642a1a923b58116146e4
javier <javier@javier-HP-Compaq-6715b-KL428LA-ABM.(none)>
2012-01-04 19:01:35 -0600

    asd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TR::Application.config.secret_token = '7524d8b4811bfa6e52e5cbafe7b666f2da6385d2a4f525cd7a9a74ebcab577b2d2324a9da11e49f7d5f80f77390e7874798483c0d134c444fc660bef5ea6701e'
